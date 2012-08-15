2338e28a6e50d28ca7ac1abd7ae151944f6d629f
Connor McSheffrey <cmcsheffrey@cmcsheffrey.local>
2012-08-15 13:24:33 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* ---------------------------------- */
+
+/*
+ *
+ * Author: Connor McSheffrey (@mcsheffrey)
+ * Organization: meltmedia (@meltmedia)
+ *
+ */
+ 
+ 
+/* ---------------------------------- */
+
+/*
+ *  TABLE OF CONTENTS
+ *
+ *  @Initialize
+ */
+ 
+/* ---------------------------------- */
+
+/* Initialize */
+
+
+gapi.client.load('API_NAME', 'API_VERSION', CALLBACK);
